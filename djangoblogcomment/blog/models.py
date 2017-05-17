f5473fbbe29132f0991dffa3cdaa1f8f0c486a04
Prithvi45 <maneprithviraj@sggs.ac.in>
2017-05-17 22:31:10 +0530

    Blog and Comment Application

---
+++ djangoblogcomment/blog/models.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager,self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+	STATUS_CHOICES = (
+		('draft', 'Draft'),('published', 'Published'),)
+
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250,unique_for_date='publish')
+	author = models.ForeignKey(User,related_name='blog_posts')
+	body = models.TextField()
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+	objects = models.Manager()
+	published = PublishedManager()
+	
+	class Meta:
+		ordering = ('-publish',)
+		def __str__(self):
+			return self.title
+
+
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, related_name='comments')
+	name = models.CharField(max_length=80)
+	email = models.EmailField()
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+	class Meta:
+		ordering = ('created',)
+	
+	def __str__(self):
+		return 'Comment by {} on {}'.format(self.name, self.post)
