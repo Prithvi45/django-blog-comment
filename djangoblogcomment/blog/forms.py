a6ff98781e980682219624b2387afff75eb4a646
Prithvi45 <maneprithviraj@sggs.ac.in>
2017-05-18 19:48:53 +0530

    comment form and view function

--- djangoblogcomment/blog/forms.py
+++ djangoblogcomment/blog/forms.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.forms import ModelForm
 from .models import Comment
 
 
--- djangoblogcomment/blog/views.py
+++ djangoblogcomment/blog/views.py
@@ -2,7 +2,7 @@
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
-
+from .forms import  CommentForm
 
 def post_list(request):
 	posts = Post.published.all()
@@ -10,9 +10,26 @@ def post_list(request):
 	return render(request,'blog/post/list.html',{'posts': posts})
 
 
+# def post_detail(request, year, month, day, post):
+# 	post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+# 	print post.id
+# 	comments = Comment.objects.filter(post_id=post.id)
+# 	print comments
+# 	return render(request,'blog/post/details.html',{'post': post})
+
+
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-	print post.id
-	comments = Comment.objects.filter(post_id=post.id)
-	print comments
-	return render(request,'blog/post/details.html',{'post': post})
+	comments = post.comments.filter(active=True)
+	if request.method == 'POST':
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			new_comment = comment_form.save(commit=False)
+			# Assign the current post to the comment
+			new_comment.post = post
+			# Save the comment to the database
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+
+	return render(request,'blog/post/details.html',{'post': post,'comments': comments,'comment_form': comment_form})
