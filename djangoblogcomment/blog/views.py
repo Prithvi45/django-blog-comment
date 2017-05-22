175394b457b8ca357294e516b6b5d2c0e485b5dd
Prithvi45 <maneprithviraj@sggs.ac.in>
2017-05-22 19:29:41 +0530

    code changes

--- djangoblogcomment/blog/views.py
+++ djangoblogcomment/blog/views.py
@@ -11,13 +11,6 @@ def post_list(request):
 	return render(request,'blog/post/list.html',{'posts': posts})
 
 
-# def post_detail(request, year, month, day, post):
-# 	post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-# 	print post.id
-# 	comments = Comment.objects.filter(post_id=post.id)
-# 	print comments
-# 	return render(request,'blog/post/details.html',{'post': post})
-
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
